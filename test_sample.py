f1a02f17f1497a29deec5b26cd34d84ec6c71131
Nína Margrét <ninamargret@Gois-MacBook-Air.local>
2020-03-04 20:05:46 +0000

    red test div 5

--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,7 @@
 import sample
 
 def test_returnes_fizzbuzz_when_divisible_by_five_and_three():
-    assert sample.Fizzbuzz(15) == "fizzbuzz"
+    assert sample.Fizzbuzz(15) == "FizzBuzz"
+
+def test_returnes_fizz_when_divisible_by_five():
+    assert sample.Fizzbuzz(5) == "Buzz"
