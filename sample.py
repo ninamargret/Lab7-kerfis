f6ebe9c711ff9f8c9d82eb6998758bb36b93f4c4
Nína Margrét <ninamargret@Gois-MacBook-Air.local>
2020-03-04 20:00:07 +0000

    green test div 5 og 3

---
+++ sample.py
@@ -0,0 +1,3 @@
+def Fizzbuzz(input):
+    if input % 5 == 0 and input % 3 == 0:
+        return "fizzbuzz"
---
+++ test_sample.py
@@ -0,0 +1,4 @@
+import sample
+
+def test_returnes_fizzbuzz_when_divisible_by_five_and_three():
+    assert sample.Fizzbuzz(15) == "fizzbuzz"
